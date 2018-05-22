d9d7ea50b0630040e5225e2b6a9c3f4e46865140
Kevin Lang <cuxa0079@zerg.medien.uni-weimar.de>
2018-05-22 11:29:52 +0200

    added and implemented new parameters for github api

--- README.md
+++ README.md
@@ -14,19 +14,24 @@ Crawler parameter:
 - **output_file**: path to the output file for project names
 - **do_clone**: should projects be cloned with git on harddrive (true) or not (false) (todo)
 - **do_zip**: should current project state be saved as zip on harddrive (true) or not (false)
+- **count**: how many projects should be crawled?
 
 Github Search Parameter:
 - **created**: creation date of the project (e.g. `>2015-01-01`)
 - **pushed**: date of last commit (e.g. `>2017-01-01`)
-- **fork**:  should the project be forked from another one? (e.g. `false`) //todo
-- **forks**:  how many forks should the project have (e.g. `>5`) //todo
-- **search_query**:  after which names should be searched (e.g. `java swing`) //todo
-- **search_location**:  where should be searched (e.g. `description`) //todo
-- **language**:  what project languages should be included (e.g. `java`)
-- **license**:  which [licence](https://help.github.com/articles/licensing-a-repository/) should the project have? //todo
-- **stars**:  how many stars should the projects have?  (e.g. `>100`)
-- **topics**:  which tags should the project have?  (e.g. `parser`) //todo
-- **archived**:  should the project be archieved?  (e.g. `false`) //todo
+- **fork**: should the project be forked from another one? (e.g. `false` or `only`)
+- **forks**: how many forks should the project have? (e.g. `>5`)
+- **search_query**: after which tags should be searched? (e.g. `swing`) //todo
+- **search_location**: should be searched in name, description or readme? (e.g. `description`)
+- **language**: what project languages should be included (e.g. `java`)
+- **license**: which [licence](https://help.github.com/articles/licensing-a-repository/) should the project have? //todo
+- **stars**: how many stars should the projects have?  (e.g. `>100`)
+- **topic**: which tag should the project have?  (e.g. `parser`)
+- **topics**: how many tags should the project have?  (e.g. `>=2`)
+- **archived**: should the project be archieved?  (e.g. `false`)
+
+If the parameter is empty in the config file, it won't be considered in the search. Possible values for queries of numbers (e.g. `100`) are: equal `100`, less `<100`, less equal `<=100`, greater `>100`, greater equal `>=100`, range `10..100`, unclosed range `100..*`. The same operators are usable with dates in the form of `YYYY-MM-DD`.
+More about [possible values for search parameters](https://help.github.com/articles/searching-repositories/) and the [search for repositories](https://developer.github.com/v3/search/#search-repositories).
 
 ## Run
 `python crawler.py crawler.conf`
@@ -46,7 +51,8 @@ square/retrofit
 ```
 
 ## TODO
-- [x] implement project name output in txt file
+- [x] use parameters for search in github api (read from config file)
+- [x] implement project name output in console or text file
 - [ ] implement git clone for projects
 - [ ] impement fetching projects as zip
 - [ ] implement all possible search criteria
--- crawler.py
+++ crawler.py
@@ -26,8 +26,6 @@ def query(url):
         response.raise_for_status()
 
 def build_query():
-    # TODO: include all search criteria
-
     query = ""
     """Call API"""
     query += 'https://api.github.com/search/'
@@ -35,20 +33,48 @@ def build_query():
     query += 'repositories?q='
 
     """Stars criteria"""
-    query += 'stars:' + stars + '+'
+    if len(stars) > 0:
+        query += 'stars:' + stars + '+'
     """Creation date criteria"""
-    query += 'created:' + created + '+'
+    if len(created) > 0:
+        query += 'created:' + created + '+'
     """Pushed date criteria"""
-    query += 'pushed:' + pushed
+    if len(pushed) > 0:
+        query += 'pushed:' + pushed + '+'
+    """Fork criteria"""
+    if len(fork) > 0:
+        query += 'fork:' + fork + '+'
+    """Forks criteria"""
+    if len(forks) > 0:
+        query += 'forks:' + forks + '+'
+    """Search_in criteria"""
+    if len(search_in) > 0:
+        query += 'in:' + search_in + '+'
+    """Language criteria"""
+    if len(language) > 0:
+        query += 'language:' + language + '+'
+    """Topic criteria"""
+    if len(topic) > 0:
+        query += 'topic:' + topic + '+'
+    """Topics criteria"""
+    if len(topics) > 0:
+        query += 'topics:' + topics + '+'
+    """Archived criteria"""
+    if len(archived) > 0:
+        query += 'archived:' + archived + '+'
+    """License criteria"""
+    if len(license) > 0:
+        query += 'license:' + license + '+'
 
     """Sorting, Order and Iteration Criteria"""
-    query += '&sort=stars&order=desc&per_page=100&page=1'
+    if query.endswith('+'):
+        query = query[:-1]
+    query += '&sort=stars&order=desc&per_page=1000&page=1'
 
     return query
 
 def find_projects():
     url = build_query()
-    print(url)
 
     response = query(url)
     result = json.loads(response.content.decode('utf-8'))
@@ -56,27 +82,39 @@ def find_projects():
     if make_repo_list and output_file:
         out = open(output_file, 'w')
 
+    i = 0
+
+    print('total number of projects: ' + str(result['total_count']))
+    print('crawling projects: ' + str(min(count,int(result['total_count']))))
+
     while True:
         """Loop over result pages until there is no 'next page' link."""
         for repo in result['items']:
-            """Save user and repo name as identifier"""
-            if make_repo_list and output_file:
-                out.write(repo['full_name'] + '\n')
-            else:
-                print(repo['full_name'])
-                sys.stdout.flush()
+            if i < count:
+                i += 1
+                """Save user and repo name as identifier"""
+                if make_repo_list and output_file:
+                    out.write(repo['full_name'] + '\n')
+                else:
+                    print(repo['full_name'])
+                    sys.stdout.flush()
 
-            """Clone repo"""
-            if do_clone:
-                # TODO: clone repo
-                print('TODO: Clone repository "' + repo['full_name'] + '"')
+                """Clone repo"""
+                if do_clone:
+                    # TODO: clone repo
+                    print('TODO: Clone repository "' + repo['full_name'] + '"')
 
-            """Save zip file of repo"""
-            if do_zip:
-                # TODO: save zip of repo
-                print('TODO: Download repository as zip "' + repo['full_name'] + '"')
+                """Save zip file of repo"""
+                if do_zip:
+                    # TODO: save zip of repo
+                    print('TODO: Download repository as zip "' + repo['full_name'] + '"')
 
-        if 'next' in response.links:
+                if i % 100 == 0:
+                    print(i)
+
+
+
+        if 'next' in response.links and i < count:
             url = response.links['next']['url']
             response = query(url)
             result = json.loads(response.content.decode('utf-8'))
@@ -94,6 +132,7 @@ if __name__ == "__main__":
     global do_clone
     global do_zip
     global output_file
+    global count
 
     global created
     global pushed
@@ -103,6 +142,7 @@ if __name__ == "__main__":
     global language
     global license
     global stars
+    global topic
     global topics
     global archived
 
@@ -124,6 +164,7 @@ if __name__ == "__main__":
     output_file = config['crawler']['output_file']
     do_clone = bool(strtobool(config['crawler']['do_clone']))
     do_zip = bool(strtobool(config['crawler']['do_zip']))
+    count = int(config['crawler']['count'])
 
     created = config['github']['created']
     pushed = config['github']['pushed']
@@ -133,6 +174,7 @@ if __name__ == "__main__":
     language = config['github']['language']
     license = config['github']['license']
     stars = config['github']['stars']
+    topic = config['github']['topic']
     topics = config['github']['topics']
     archived = config['github']['archived']
 
